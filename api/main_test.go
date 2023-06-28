c991f0ecf355cc55929c7e5cf168630254fe174b
Shivshankar Manivasagam <shivshankarmanivasagam@shivsharsmbp287.video54.local>
2023-06-28 16:06:51 -0700

    Add login user API with token:

--- api/main_test.go
+++ api/main_test.go
@@ -3,11 +3,29 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	db "github.com/shivshankarm/bankservice/db/sqlc"
+	"github.com/shivshankarm/bankservice/util"
+	"github.com/stretchr/testify/require"
+
 	_ "github.com/lib/pq"
+
 	_ "github.com/shivshankarm/bankservice/util"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
